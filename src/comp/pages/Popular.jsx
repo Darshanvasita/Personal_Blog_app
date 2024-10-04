4399f9fd9d14a9666df9c3e558e2ef9078b5e3eb
DarshanVasita <d.s.vasita1209@gmail.com>
2024-10-04 14:14:35 +0530

    update

--- src/comp/pages/Popular.jsx
+++ src/comp/pages/Popular.jsx
@@ -9,13 +9,13 @@ const Popular = (props) => {
       <div className="container-fluid  justify-content-center mt-5"> 
         <div className="d-flex justify-content-center">
           <div className="card shadow border-1 mb-3" style={{minWidth:"270px", minHeight:"50px"}} >
-            <img src="public/img/Popular.png" alt="" style={{width:"30px"}} className='position-absolute mx-2 my-2' />
+            <img src="https://res.cloudinary.com/diyqncua4/image/upload/v1728020171/Blogtastic/xzlzs8lcksn1haopddgm.png" alt="" style={{width:"30px"}} className='position-absolute mx-2 my-2' />
             <img src={props.Imgs} alt="dummy" height={300} style={{minHeight:"200px"}} className='position-relative' />
             <div className="card-body bg-black text-white">
               <h3>{props.title}</h3>
               <div className='data d-flex align-items-center'>  
                 <div className="auth bg-secondary rounded-pill"> 
-                  <img src="public/img/writer.png" alt="" width={'25px'}/>
+                  <img src="https://res.cloudinary.com/diyqncua4/image/upload/v1728020172/Blogtastic/xlzd9t5kh1z2mjqiriex.png" alt="" width={'25px'}/>
                 </div>
                 <span className='mx-2'>{props.b_writer}</span>
                 <span className='mx-2'>{props.category}</span>                                              
